868a32c7ebdb0cc39de8082474082a7417254baa
JuanSatizabal2006 <juansatizabal2701@gmail.com>
2024-11-26 17:36:14 -0500

    aaaaaa

---
+++ front/src/components/titleQuiz/titleQuiz.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TitleQuiz = () => {
+  return (
+    <header class="container_title">
+        <h1 class="title_quiz">¡Adivina el Pokemon!</h1>
+    </header>
+  )
+}
+
+export default TitleQuiz
--- front/src/sections/navQuiz/navQuiz.jsx
+++ front/src/sections/navQuiz/navQuiz.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const NavQuiz = () => {
-  return (
-    <div>NavQuiz</div>
-  )
-}
 
-export default NavQuiz
+  return (
+    <header class="container_header">
+      {/*<!--Informacion superior con el puntaje y la opcion de regresar al inicio-->*/}
+      <p class="item_puntaje">
+        Puntaje: <span class="item_puntaje_num">0</span>
+      </p>
+      <i class="fa-solid fa-house item_puntaje" id="icon_again"></i>
+    </header>
+  );
+};
+
+export default NavQuiz;
